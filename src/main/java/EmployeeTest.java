c8aafe2d917264002d94e7a65ddc3ab8c0cd3e59
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2024-12-30 10:32:05 +0000

    Edit 1

---
+++ src/main/java/EmployeeTest.java
@@ -0,0 +1,8 @@
+
+public class EmployeeTest{
+  public static void main(String[] args){
+    Employee e = new Employee("John", 1000, 2000, 1, 1);
+    System.out.println(e.getName());
+    System.out.println(e.getSalary());
+  }
+}
